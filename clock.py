2996e69fcc5d2ce37688abe9fbdf900880fbab2c
Daniel <danielpicone2@gmail.com>
2019-02-17 22:40:32 +1100

    Working scheduler

--- clock.py
+++ clock.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import main
+import datetime
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour = 11)
-@sched.scheduled_job('interval', minutes = 1)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour = 11)
 def scheduled_job():
     main.update_stock_history()
     print("Stock history has been updated for ", datetime.datetime.today().strftime('%Y-%m-%d'))
