e98d33e8ed77aeaa2c5a861dc3619877a9f55b80
Daniel <danielpicone2@gmail.com>
2019-02-20 18:22:40 +1100

    add sendgrid functionality

--- clock.py
+++ clock.py
@@ -4,10 +4,10 @@ import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour = 11)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour = 10)
 def scheduled_job():
     main.update_stock_history()
-    print("Stock history has been updated for ", datetime.datetime.today().strftime('%Y-%m-%d'))
+    print("Stock history has been updated for", datetime.datetime.today().strftime('%Y-%m-%d'))
 
 # @sched.scheduled_job('interval', minutes = 1)
 # def time_job():
