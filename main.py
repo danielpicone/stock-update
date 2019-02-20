5b6fe260cc141434ac9593357247dcc9f1c6bac6
Daniel <danielpicone2@gmail.com>
2019-02-20 19:43:47 +1100

    make sendgrid a function

--- main.py
+++ main.py
@@ -41,14 +41,15 @@ def update_stock_history():
             cell.value = stock_tuple[i][cell.col-2]
     history_sheet.update_cells(new_cells)
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("danielpicone2@gmail.com")
-to_email = Email("danielpicone2@gmail.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
-# import pdb; pdb.set_trace()
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+def send_email():
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("danielpicone2@gmail.com")
+    to_email = Email("danielpicone2@gmail.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, subject, to_email, content)
+    # import pdb; pdb.set_trace()
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
