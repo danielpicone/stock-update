aa4f33e98cefd5e3680d57cefc48f806c10ac326
Daniel <danielpicone2@gmail.com>
2019-03-01 00:57:41 +1100

    Remove email attachment testing

--- clock.py
+++ clock.py
@@ -4,15 +4,15 @@ import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour = 10)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour = 9)
 def update_schedule():
     main.update_stock_history()
     print("Stock history has been updated for", datetime.datetime.today().strftime('%Y-%m-%d'))
 
-# @sched.scheduled_job('cron', day_of_week='fri', hour = 10)
-@sched.scheduled_job('interval', minutes = 1)
+@sched.scheduled_job('cron', day_of_week='fri', hour = 9)
+# @sched.scheduled_job('interval', minutes = 1)
 def email_schedule():
-    main.generate_email("Test.pdf")
+    main.generate_email("Stock report.pdf")
     print("Email was sent at ", datetime.datetime.today().strftime('%Y-%m-%d %H:%M'))
 
 # @sched.scheduled_job('interval', minutes = 1)
